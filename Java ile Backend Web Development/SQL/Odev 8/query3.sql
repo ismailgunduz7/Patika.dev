0152b9cb719b85a5a5aa233e62b10994743bf28c
Ismail <isgun7@gmail.com>
2021-09-15 18:51:34 +0400

    SQL - Odev 8 completed

---
+++ Java ile Backend Web Development/SQL/Odev 8/query3.sql
@@ -0,0 +1,21 @@
+UPDATE employee
+SET email='townody1995@anymail.com'
+WHERE id=41;
+
+UPDATE employee
+SET id=0
+WHERE name='Zora Braney';
+
+UPDATE employee
+SET name='Nick Sutherden'
+WHERE birthday='1972-06-14';
+
+UPDATE employee
+SET birthday='1991-06-04'
+WHERE email='aglason9@nbcnews.com';
+
+UPDATE employee
+SET name='Ismail Gunduz',
+	birthday='1999-09-10',
+	email='isgun7@gmail.com'
+WHERE id=7;
---
+++ Java ile Backend Web Development/SQL/Odev 8/query4.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee
+WHERE id=31;
+
+DELETE FROM employee
+WHERE name='Rogerio De Cruz';
+
+DELETE FROM employee
+WHERE birthday='2000-01-05';
+
+DELETE FROM employee
+WHERE email='kfaltin11@gnu.org';
+
+DELETE FROM employee
+WHERE id=13;
